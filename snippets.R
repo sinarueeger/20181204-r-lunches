0066ced5a22f809ffcf96aa9c0ca9b37f4ed36eb
Sina Rüeger <sina.rueeger@gmail.com>
2018-11-29 11:44:35 +0100

    initiating snippet

--- snippets.R
+++ snippets.R
@@ -1,3 +1,21 @@
 
 
-## 
+## Data managment: Create the data you wish to see in the world
+## - variable names
+## - codebook
+## - analysis friendly data
+
+## Code
+## - styleguides: give variables and functions meaningful names
+## - commenting style
+## - decompose code into functions
+
+## How to implement habits: gist.github.com
+
+## Project organisation
+## - Directory structur: ProjectTemplate, name yourself, lockedata
+## - runall script
+
+## Use git
+
+## Work with others
