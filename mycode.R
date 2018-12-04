63f140e141a5b5dd5ba504ec31273f4754408116
Sina Rueeger <sina.rueeger@gmail.com>
2018-12-04 07:57:09 +0100

    small typos

--- mycode.R
+++ mycode.R
@@ -3,7 +3,6 @@ cols <- c("#88398A","#B07FB2","#F7CCA4","#C8C05E","#627C63") ## generated here: 
 
 ## colors
 set.seed(3)
-N=2000
 df=data.frame(x=runif(N), y =runif(N), col = sample(cols, N, replace = TRUE), size=runif(N, 1, 10))
 
 ## no border
