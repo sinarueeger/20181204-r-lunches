45d6d65e096b1db02a4689d31807f969ce0751a3
Sina Rueeger <sina.rueeger@gmail.com>
2018-12-04 15:04:10 +0100

    fixing some typos

--- mycode.R
+++ mycode.R
@@ -7,5 +7,6 @@ df=data.fame(x=runif(N), y =runif(N), col = sample(cols, N, replace = TRUE), siz
 
 ## no border
 qp <- gqplot(df, aes(x, y))+ geom_point(aes(size =size, fill =I(col), color = I(col)), alpha = 0.4, shape = 16)+theme_void()+theme(legend.position="none")+scale_size_continuous(range = c(1, 30))
+
 return(qp)
 }
